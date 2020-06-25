a6d961512befaa4c5f087f23d80ce23f82bfc8a3
maratsod <sodnopov@list.ru>
2020-06-25 17:25:42 +0800

    4.1.2. Глупый бот - тесты [#310672]

---
+++ test/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,47 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class DummyBotTest {
+    @Test
+    public void whenGreetBot() {
+        assertThat(
+                DummyBot.answer("Привет, Бот."),
+                is("Привет, умник.")
+        );
+    }
+
+    @Test
+    public void whenByeBot() {
+        assertThat(
+                DummyBot.answer("Пока."),
+                is("До скорой встречи.")
+        );
+    }
+
+    @Test
+    public void whenUnknownBot() {
+        assertThat(
+                DummyBot.answer("Сколько будет 2 + 2?"),
+                is("Это ставит меня в тупик. Задайте другой вопрос.")
+        );
+    }
+
+    @Test
+    public void whenUnknownBot1() {
+        assertThat(
+                DummyBot.answer("Как тебя зовут?"),
+                is("Это ставит меня в тупик. Задайте другой вопрос.")
+        );
+    }
+
+    @Test
+    public void whenUnknownBot2() {
+        assertThat(
+                DummyBot.answer("Пака"),
+                is("Это ставит меня в тупик. Задайте другой вопрос.")
+        );
+    }
+}
