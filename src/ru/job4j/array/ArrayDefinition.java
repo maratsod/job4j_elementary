0366814de9a42e0f38398e2c2643895cc81ef412
maratsod <sodnopov@list.ru>
2020-07-01 18:28:30 +0800

    6.0.2. Заполнение массива. [#310700]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Ivan Ivanov";
+        names[2] = "Petr Petrov";
+        names[3] = "Vasya Vasilev";
+        for (int i = 0; i < 4; i++) {
+            System.out.println(names[i]);
+        }
+
     }
 }
