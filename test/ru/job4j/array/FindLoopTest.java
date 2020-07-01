571138384e9383978952007370b4489ccc7065e6
maratsod <sodnopov@list.ru>
2020-07-01 20:52:55 +0800

    6.1. Классический поиск перебором. [#310686]

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -32,4 +32,13 @@ public class FindLoopTest {
         assertThat(result, is(expect));
     }
 
+    @Test
+    public void whenArrayHas3Then1() {
+        int[] input = {5, 2, 100, 4};
+        int value = 3;
+        int result = FindLoop.indexOf(input, value);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
 }
