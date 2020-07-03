c694dff23119d5fbbe829f5fbf219f8b13158a72
maratsod <sodnopov@list.ru>
2020-07-03 13:32:09 +0800

    6.4.1. Сравнить последние элементы двух массивов [#310707]

--- src/ru/job4j/array/EqLast.java
+++ src/ru/job4j/array/EqLast.java
@@ -2,14 +2,6 @@ package ru.job4j.array;
 
 public class EqLast {
     public static boolean check(int[] left, int[] right) {
-
-        for (int i = left.length -1; 0 < i; i--) {
-            for (int j = right.length -i; 0 < j; j--) {
-                if (left[i] == right[i]) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return left[left.length - 1] == right[right.length - 1];
 }
 }
