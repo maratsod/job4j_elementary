57caf16abcedf2cd92b4baa73985f8fb45f5e079
maratsod <sodnopov@list.ru>
2021-07-15 18:21:55 +0800

    2. Добавление Maven в существующий проект. [#483536]

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,37 @@
+package ru.job4j.calculator;
+import static ru.job4j.math.MathFunction.*;
+import static ru.job4j.math.MathFunction.sum;
+
+public class MathCalculator {
+
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double subAndDiv(double first, double second) {
+        return sub(first, second)
+                + div(first, second);
+    }
+
+    public static double allMethods(double first, double second) {
+        return sub(first, second)
+                + sum(first, second) + div(first, second)
+                + multiply(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен: " + subAndDiv(30, 20));
+        System.out.println("Результат расчета равен: " + allMethods(40, 20));
+    }
+}
+
+
+/*
+один будет считать сумму двух операций из класса MathFunction: разность двух чисел и деление двух чисел.
+
+- второй метод будет рассчитывать сумму вычисления всех четырех операций из класса MathFunction.
+
+
+ */
